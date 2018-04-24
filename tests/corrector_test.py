096b7b2df02303563972d668124c99ec49fb6262
BinNong <bin_nong@foxmail.com>
2018-04-24 14:57:40 +0800

    Update corrector_test.py

--- tests/corrector_test.py
+++ tests/corrector_test.py
@@ -13,19 +13,19 @@ class BasicTestSuite(unittest.TestCase):
     @staticmethod
     def test_text1():
         error_sentence_1 = '机七学习是人工智能领遇最能体现智能的一个分知'
-        correct_sent = correct(error_sentence_1)
+        correct_sent = corrector.correct(error_sentence_1)
         print("original sentence:{} => correct sentence:{}".format(error_sentence_1, correct_sent))
 
     @staticmethod
     def test_text2():
         error_sentence_2 = '杭洲是中国的八大古都之一，因风景锈丽，享有“人间天棠”的美誉！'
-        correct_sent = correct(error_sentence_2)
+        correct_sent = corrector.correct(error_sentence_2)
         print("original sentence:{} => correct sentence:{}".format(error_sentence_2, correct_sent))
 
     @staticmethod
     def test_text3():
         error_sentence_3 = '我们现今所"使用"的大部分舒学符号，你们用的什么婊点符号'
-        correct_sent = correct(error_sentence_3)
+        correct_sent = corrector.correct(error_sentence_3)
         print("original sentence:{} => correct sentence:{}".format(error_sentence_3, correct_sent))
 
     @staticmethod
@@ -52,7 +52,7 @@ class BasicTestSuite(unittest.TestCase):
             '还有我要看他们的个性，如果跟同时合不来受到压力的话，无法专心地工作。',
         ]
         for line in error_sentences:
-            correct_sent = correct(line)
+            correct_sent = corrector.correct(line)
             print("original sentence:{} => correct sentence:{}".format(line, correct_sent))
 
     @staticmethod
